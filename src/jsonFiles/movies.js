6248ceaf92968c714613fe07de6cdfe81fa0f0ba
tandrima3101 <goswami3101@gmail.com>
2023-09-14 21:02:09 +0530

    inital commit

---
+++ src/jsonFiles/movies.js
@@ -0,0 +1,122 @@
+export const movies = [
+    {
+        "index" : 1,
+        "movieName" : "2001: A Space Odyssey",
+        "rating" : "5/5",
+        "releasedDate" : "1968"
+    },
+    {
+        "index" : 2,
+        "movieName" : "The Godfather",
+        "rating" : "5/5",
+        "releasedDate" : "1972"
+    },
+    {
+        "index" : 3,
+        "movieName" : "Citizen Kane",
+        "rating" : "4/5",
+        "releasedDate" : "1941"
+    },
+    {
+        "index" : 4,
+        "movieName" : "Seven Samurai",
+        "rating" : "3/5",
+        "releasedDate" : "1954"
+    },
+    {
+        "index" : 5,
+        "movieName" : "The Avengers",
+        "rating" : "3.5/5",
+        "releasedDate" : "2011"
+    },
+    {
+        "index" : 6,
+        "movieName" : "IronMan",
+        "rating" : "4/5",
+        "releasedDate" : "2008"
+    },
+    {
+        "index" : 7,
+        "movieName" : "SpiderMan",
+        "rating" : "5/5",
+        "releasedDate" : "2002"
+    },
+    {
+        "index" : 8,
+        "movieName" : "Avengers:Age of Ultron",
+        "rating" : "3/5",
+        "releasedDate" : "2013"
+    },
+    {
+        "index" : 9,
+        "movieName" : "Suzume",
+        "rating" : "5/5",
+        "releasedDate" : "2023"
+    },
+    {
+        "index" : 10,
+        "movieName" : "Demon Slayer",
+        "rating" : "5/5",
+        "releasedDate" : "2023"
+    },
+    {
+        "index" : 11,
+        "movieName" : "Conguring",
+        "rating" : "5/5",
+        "releasedDate" : "2016"
+    },
+    {
+        "index" : 12,
+        "movieName" : "NUN",
+        "rating" : "1/5",
+        "releasedDate" : "2020"
+    },
+    {
+        "index" : 13,
+        "movieName" : "Shawshank Redemption",
+        "rating" : "5/5",
+        "releasedDate" : "1998"
+    },
+    {
+        "index" : 14,
+        "movieName" : "Shutter Island",
+        "rating" : "5/5",
+        "releasedDate" : "2000"
+    },
+    {
+        "index" : 15,
+        "movieName" : "The greatest Showman",
+        "rating" : "4/5",
+        "releasedDate" : "2007"
+    },
+    {
+        "index" : 16,
+        "movieName" : "Prestige",
+        "rating" : "5/5",
+        "releasedDate" : "2010"
+    },
+    {
+        "index" : 17,
+        "movieName" : "Tenet",
+        "rating" : "3/5",
+        "releasedDate" : "2019"
+    },
+    {
+        "index" : 18,
+        "movieName" : "Menu",
+        "rating" : "2/5",
+        "releasedDate" : "2022"
+    },
+    {
+        "index" : 19,
+        "movieName" : "Chef",
+        "rating" : "1/5",
+        "releasedDate" : "2002"
+    },
+    {
+        "index" : 20,
+        "movieName" : "Nantsu No Taizai",
+        "rating" : "4.5/5",
+        "releasedDate" : "2015"
+    }
+]
